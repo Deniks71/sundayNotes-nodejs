45db9d952239d962e398f1d387102967aee9a092
Denilson Nascimento <denilsonn7187@gmail.com>
2024-11-02 21:10:14 -0300

    Inserindo novos elementos no codigo

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json())
 
+app.post('/api/sundayNotes/register', (req,res) => {
+    const {name,email} = req.body;
 
+    res.status(200).json({name,
+        email
+    })
+})
 
 
 export default app;
