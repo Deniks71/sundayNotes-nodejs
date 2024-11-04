933670de8e4032d30660e9262d3060626422c041
Denilson Nascimento <denilsonn7187@gmail.com>
2024-11-04 20:41:28 -0300

    Configurando variaveis de ambiente

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 import express from 'express';
 
+
 const app = express();
 
 app.use(express.json())
 
+//Route to send the name and email of the user.
 app.post('/api/sundayNotes/register', (req,res) => {
     const {name,email} = req.body;
-
+  
     res.status(200).json({name,
         email
     })
 })
+
 
 
 export default app;
