4e49e18cc244e9de91e1c318a090abf03a9f0c85
Denilson Nascimento <denilsonn7187@gmail.com>
2024-11-07 23:09:29 -0300

    Inserindo criaçao e persistencia de dados do usuario no db

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,9 +19,16 @@ export async function addUserEmail (req,res){
 
         const [emailExists] = await pool.execute(checkEmailSql,[email]);
 
+        //Checa se o email informado já existe no banco de dadosS
         if(emailExists[0]){
             return res.status(422).json({msg:"Email ja cadastrado, por favor insira outro Email."})
         };
+
+        const createUserSql = "INSERT INTO usuarios (`nome`, `email`) VALUES (?, ?);"
+        const usuario = [name,email]
+
+        const [result,fields] = await pool.execute(createUserSql,usuario);
+        console.log(result,fields)
 
         return res.status(200).json({name,email});
 
